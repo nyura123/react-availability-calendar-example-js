f18897f0893b4339ae6de7fda63b55c07e02e158
nyura123 <nyura123@gmail.com>
2020-04-04 23:28:33 -0500

    react-availability-calendar example App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo } from "react";
 
-function App() {
+import {
+  AvailabilityCalendar,
+  // AvailabilityEvent,
+  // MsSinceMidnightRange,
+  // Booking,
+  // Range,
+  defaultComponents,
+} from "react-availability-calendar";
+import moment from "moment";
+
+// import "bootstrap/dist/css/bootstrap.min.css";
+import "./custom.scss";
+// import { Overrides } from "react-availability-calendar/dist/overrides";
+
+const msInHour = 60 * 60 * 1000;
+
+const App = () => {
+  const now = new Date();
+
+  const onAvailabilitySelected = (a) =>
+    console.log("Availability slot selected: ", a);
+
+  const onChangedCalRange = (r /*: Range*/) =>
+    console.log("Calendar range selected (fetch bookings here): ", r);
+
+  const providerTimeZoneForBlockOutHours = "America/New_York";
+  const blockOutPeriods /*: MsSinceMidnightRange[]*/ = [
+    [0 * msInHour, 9 * msInHour],
+    [19 * msInHour, 24 * msInHour],
+  ];
+
+  const bookings /*: Booking[]*/ = [
+    {
+      startDate: new Date(2020, 2, 1, 19),
+      endDate: new Date(2020, 2, 1, 20),
+    },
+    {
+      startDate: new Date(2020, 2, 1, 16, 30),
+      endDate: new Date(2020, 2, 1, 17),
+    },
+  ];
+
+  // Optional overrides to tweak appearance of various components
+  const overrides = useMemo(
+    /*<Overrides>*/
+    () => ({
+      ...defaultComponents,
+      // ToolBar: { Root: (p: any) => <div>{JSON.stringify(p)}</div> },
+      ToolBar: {
+        className: "border btn-group",
+        style: { minHeight: undefined },
+      },
+      ToolBarButton: {
+        className: "btn btn-outline-info",
+        style: { outline: "none" },
+      },
+      Weekday: {
+        style: {
+          borderWidth: 0,
+          borderStyle: "solid",
+          borderBottomWidth: 1,
+          borderRightWidth: 1,
+          borderColor: "#dddddd",
+        },
+        className: "none",
+      },
+      DayCell: {
+        style: (p) =>
+          p.isSelected
+            ? { transition: "width 200ms, height 200ms", height: 60, width: 60 }
+            : { transition: "width 200ms, height 200ms" },
+        className: (p) =>
+          p.isSelected
+            ? "rounded-circle border-success"
+            : p.hasAvail
+            ? "rounded-circle border-primary"
+            : "rounded-circle border-secondary",
+      },
+    }),
+    []
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: 350 }}>
+      <AvailabilityCalendar
+        bookings={bookings}
+        providerTimeZone={providerTimeZoneForBlockOutHours}
+        moment={moment}
+        initialDate={now}
+        onAvailabilitySelected={onAvailabilitySelected}
+        onCalRangeChange={onChangedCalRange}
+        blockOutPeriods={blockOutPeriods}
+        overrides={overrides}
+      />
     </div>
   );
-}
+};
 
 export default App;
