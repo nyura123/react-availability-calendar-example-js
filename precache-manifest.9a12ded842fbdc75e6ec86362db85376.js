da354a2ed6ff166db166541770c6069e6a6958d4
nyura123 <nyura123@gmail.com>
2020-04-05 00:00:10 -0500

    Updates

---
+++ precache-manifest.9a12ded842fbdc75e6ec86362db85376.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ab8f96c5b45b16328deb64989f2fe5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dccd4744a0e79f08d0ca",
+    "url": "/static/css/main.fee896f8.chunk.css"
+  },
+  {
+    "revision": "cb99e9d120f9aa6f5992",
+    "url": "/static/js/2.7a714ada.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7a714ada.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dccd4744a0e79f08d0ca",
+    "url": "/static/js/main.f6c9ebd6.chunk.js"
+  },
+  {
+    "revision": "ce5ac2bf027176c91aba",
+    "url": "/static/js/runtime-main.0649fdeb.js"
+  }
+]);
